090a0efd78de86ffd001a0a7762c7b571efb849a
holychan <east0321@outlook.kr>
2025-08-05 14:16:11 +0900

    Feat: BusinessException 에 생성자 추가

--- wise-spending-life/src/main/java/com/wisespendinglife/wise_spending_life/global/error/BusinessException.java
+++ wise-spending-life/src/main/java/com/wisespendinglife/wise_spending_life/global/error/BusinessException.java
@@ -9,11 +9,16 @@ import org.springframework.http.HttpStatus;
  * 도메인 계층에서 throw 하는 커스텀 런타임 예외의 베이스 클래스.
  */
 @Getter
-@AllArgsConstructor
 public class BusinessException extends RuntimeException {
     private final HttpStatus httpStatus;  // HTTP 상태코드
     private final String errorCode;  // 에러 코드
     private final String errorMessage;  // 에러 메시지(정확한)
 
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.httpStatus   = errorCode.getHttpStatus();
+        this.errorCode    = errorCode.getCode();
+        this.errorMessage = errorCode.getMessage();
+    }
 
 }
