110d310e1d746e723e40b17dc4784817f8ddb2a0
holychan <east0321@outlook.kr>
2025-08-12 18:21:40 +0900

    Test

--- wise-spending-life/src/main/java/com/wisespendinglife/wise_spending_life/domain/category/entity/CategoryType.java
+++ wise-spending-life/src/main/java/com/wisespendinglife/wise_spending_life/domain/category/entity/CategoryType.java
@@ -1,5 +1,5 @@
 package com.wisespendinglife.wise_spending_life.domain.category.entity;
 
 public enum CategoryType {
-    PAYMENT, CHALLENGE, STORE, NOTIFICATION
+    PAYMENT, CHALLENGE, STORE, NOTIFICATION, TEST
 }
