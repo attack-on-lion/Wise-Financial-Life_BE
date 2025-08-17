bda579ffa112ebd8328297efc443656dab7d4493
holychan <east0321@outlook.kr>
2025-08-18 02:25:24 +0900

    Feat: 캐릭터 Repository 구현

---
+++ wise-spending-life/src/main/java/com/wisespendinglife/wise_spending_life/domain/character/repository/CharacterRepository.java
@@ -0,0 +1,8 @@
+package com.wisespendinglife.wise_spending_life.domain.character.repository;
+import com.wisespendinglife.wise_spending_life.domain.character.entity.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long> {
+}
