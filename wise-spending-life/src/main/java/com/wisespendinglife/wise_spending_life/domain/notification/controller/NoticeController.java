6be6ea06c6d9a91f26e1502b3449123585346201
holychan <east0321@outlook.kr>
2025-08-21 20:47:32 +0900

    Feat: Notification Controller 구현

---
+++ wise-spending-life/src/main/java/com/wisespendinglife/wise_spending_life/domain/notification/controller/NoticeController.java
@@ -0,0 +1,56 @@
+package com.wisespendinglife.wise_spending_life.domain.notification.controller;
+
+import com.wisespendinglife.wise_spending_life.domain.notification.dto.NoticeRequestDto;
+import com.wisespendinglife.wise_spending_life.domain.notification.dto.NoticeResponseDto;
+import com.wisespendinglife.wise_spending_life.domain.notification.dto.NotificationType;
+import com.wisespendinglife.wise_spending_life.domain.notification.service.NoticeCommandService;
+import com.wisespendinglife.wise_spending_life.domain.notification.service.NoticeQueryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Slf4j
+public class NoticeController {
+
+    private final NoticeCommandService noticeCommandService;
+    private final NoticeQueryService noticeQueryService;
+
+    @GetMapping("/users/{user_id}/notifications")
+    public ResponseEntity<NoticeResponseDto.Notices> getNotifications(
+            @PathVariable("user_id") Long userId,
+            @RequestParam(required = false) NotificationType category,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "15") int size
+    ) {
+
+        log.info(">>> [CTRL] /api/users/{}/notifications?category={}, page={}, size={}", userId, category, page, size);
+
+        return ResponseEntity.ok(noticeQueryService.getNotices(userId, category, page, size));
+
+    }
+
+    @PostMapping("/api/notifications")
+    public ResponseEntity<NoticeResponseDto.NoticCreateResponseDto> addNotification(
+            @Validated @RequestBody NoticeRequestDto.CreateNoticDto requestDto
+    ) {
+
+        log.info(">>> [CTRL] /api/notifications POST -> {}", requestDto);
+
+        return ResponseEntity.ok(noticeCommandService.createNotice(requestDto));
+    }
+
+    @DeleteMapping("/api/notifications/{notice_id}")
+    public ResponseEntity<NoticeResponseDto.NoticeDeleteResponseDto> deleteNotification(
+            @PathVariable("notice_id") Long noticeId
+    ) {
+        
+        log.info(">>> [CTRL] /api/notifications/{} DELETE", noticeId);
+
+        return ResponseEntity.ok(noticeCommandService.deleteNotice(noticeId));
+    }
+}
